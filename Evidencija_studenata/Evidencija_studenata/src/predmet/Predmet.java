7a035a08c5923f2dada2cde32c6c64580a2ee080
Radislav <radislavkosijer@gmail.com>
2022-11-21 13:23:47 +0100

    Issue #16: Dodavanje predmeta
    -Napravljena klasa Predmet sa atributima

---
+++ Evidencija_studenata/Evidencija_studenata/src/predmet/Predmet.java
@@ -0,0 +1,38 @@
+package predmet;
+
+
+public class Predmet {
+	
+	String ime;
+	int ECTS, sifra;
+	
+	Predmet(String ime, int ECTS, int sifra){
+		this.ime=ime;
+		this.ECTS=ECTS;
+		this.sifra=sifra;
+	}
+	@Override
+	public String toString() {
+		return "Predmet: "+ime+"\nBroj ECTS bodova: "+ ECTS+ "\nSifra predmeta: "+sifra; 
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public int getECTS() {
+		return ECTS;
+	}
+	public void setECTS(int eCTS) {
+		ECTS = eCTS;
+	}
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	
+}
+
