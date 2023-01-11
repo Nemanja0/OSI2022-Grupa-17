d2e8947922c9aead948de8f1d2635956332781ad
Nemanja0 <ncenic39@gmail.com>
2023-01-11 22:12:32 +0100

    Issue #34 : Kreiranje administratorskih i klijentskih naloga - U klasi RegularUser.java uradjena je mala prepravka - U klasi Administrator.java dodata je metoda createAdministratorClientAccount() koja sluzi za kreiranje naloga - U klasi Main.java modifikovana je metoda register() kako bi se izbjeglo dupliranje koda

--- Aplikacija/Softver_za_prodaju_ulaznica/src/users/Administrator.java
+++ Aplikacija/Softver_za_prodaju_ulaznica/src/users/Administrator.java
@@ -1,8 +1,17 @@
 package users;
 
+import main.Main;
+
 public class Administrator extends User {
 	
 	public Administrator(String name, String surname, String username, String password) {
 		super(name,surname,username,password);
+	}
+	
+	public void createAdministratorClientAccount() {
+		if(Main.register())
+			System.out.println("Account successfuly created!");
+		else
+			System.out.println("Account has not been created!");
 	}
 }
